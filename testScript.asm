83302fe6b3479a2b615618bdb83490a15a1d1cb2
Nenthor <david@nentwich.dev>
2025-03-07 11:13:24 +0100

    Changed testScript

--- testScript.asm
+++ testScript.asm
@@ -6,31 +6,69 @@
 	.globl	@feat.00
 .set @feat.00, 0
 	.file	"testScript"
-	.def	printChar;
+	.def	printYes;
 	.scl	2;
 	.type	32;
 	.endef
-	.globl	printChar
+	.globl	printYes
 	.p2align	4, 0x90
-printChar:
-.seh_proc printChar
-	subq	$56, %rsp
-	.seh_stackalloc 56
+printYes:
+.seh_proc printYes
+	subq	$40, %rsp
+	.seh_stackalloc 40
 	.seh_endprologue
-	cmpl	$6, current(%rip)
-	jl	.LBB0_2
-	movb	$89, 46(%rsp)
-	movl	$0, 48(%rsp)
-	leaq	46(%rsp), %rcx
-	jmp	.LBB0_3
-.LBB0_2:
-	movb	$78, 47(%rsp)
-	movl	$0, 52(%rsp)
-	leaq	47(%rsp), %rcx
-.LBB0_3:
+	movb	$89, 33(%rsp)
+	movb	$101, 34(%rsp)
+	movb	$115, 35(%rsp)
+	movl	$0, 36(%rsp)
+	leaq	33(%rsp), %rcx
 	callq	puts
 	nop
-	addq	$56, %rsp
+	addq	$40, %rsp
+	retq
+	.seh_endproc
+
+	.def	printNo;
+	.scl	2;
+	.type	32;
+	.endef
+	.globl	printNo
+	.p2align	4, 0x90
+printNo:
+.seh_proc printNo
+	subq	$40, %rsp
+	.seh_stackalloc 40
+	.seh_endprologue
+	movb	$78, 34(%rsp)
+	movb	$111, 35(%rsp)
+	movl	$0, 36(%rsp)
+	leaq	34(%rsp), %rcx
+	callq	puts
+	nop
+	addq	$40, %rsp
+	retq
+	.seh_endproc
+
+	.def	printCheck;
+	.scl	2;
+	.type	32;
+	.endef
+	.globl	printCheck
+	.p2align	4, 0x90
+printCheck:
+.seh_proc printCheck
+	subq	$40, %rsp
+	.seh_stackalloc 40
+	.seh_endprologue
+	cmpl	$4, test(%rip)
+	jg	.LBB2_2
+	callq	printYes
+	jmp	.LBB2_3
+.LBB2_2:
+	callq	printNo
+.LBB2_3:
+	nop
+	addq	$40, %rsp
 	retq
 	.seh_endproc
 
@@ -45,25 +83,23 @@ main:
 	subq	$40, %rsp
 	.seh_stackalloc 40
 	.seh_endprologue
-	movl	$0, 36(%rsp)
-	cmpl	$9, 36(%rsp)
-	jg	.LBB1_3
+	cmpl	$9, test(%rip)
+	jg	.LBB3_3
 	.p2align	4, 0x90
-.LBB1_2:
-	incl	current(%rip)
-	callq	printChar
-	incl	36(%rsp)
-	cmpl	$9, 36(%rsp)
-	jle	.LBB1_2
-.LBB1_3:
+.LBB3_2:
+	callq	printCheck
+	incl	test(%rip)
+	cmpl	$9, test(%rip)
+	jle	.LBB3_2
+.LBB3_3:
 	xorl	%eax, %eax
 	addq	$40, %rsp
 	retq
 	.seh_endproc
 
-	.data
-	.weak	current
+	.bss
+	.weak	test
 	.p2align	2, 0x0
-current:
-	.long	1
+test:
+	.long	0
 
